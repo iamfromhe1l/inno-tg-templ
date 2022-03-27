d000589eca58fadea81a37d56bb7bbdeab3b560d
iamfromhe1l <sgadgsfad@gmail.com>
2022-03-27 17:40:49 +0300

    fuck

--- main1.py
+++ main1.py
@@ -6,7 +6,6 @@ from aiogram.dispatcher.webhook import SendMessage
 from aiogram.utils.executor import start_webhook
 import os
 
-
 TOKEN = os.getenv('BOT_TOKEN')
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
