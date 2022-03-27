b35e8a575933be1568bf7597197ee3ae2a77b754
iamfromhe1l <sgadgsfad@gmail.com>
2022-03-27 15:12:19 +0300

    frscom

---
+++ template.py
@@ -0,0 +1,52 @@
+import logging
+from aiogram import Bot, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher.webhook import SendMessage
+from aiogram.utils.executor import start_webhook
+import config
+import os
+
+TOKEN = os.getenv('BOT_TOKEN')
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+wh_host = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+wh_path = f'/webhook/{TOKEN}'
+wh_url = f'{wh_host}{wh_path}'
+
+
+whapp_host = '0.0.0.0'
+whapp_port = os.getenv('PORT', default=8000)
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
+
+@dp.message_handler()
+async def echo(ms:types.Message):
+    return SendMessage(ms.chat.id, ms.text)
+
+async def on_startup(dp):
+    await bot.set_webhook((wh_url))
+    
+async def on_shutdown(dp):
+    logging.warning('turning off')
+    
+    await bot.delete_webhook()
+    await dp.storage.close()
+    await dp.storage.wait_close()
+    
+    logging.warning('bye')
+    
+if __name__ == '__main__':
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=wh_path,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=whapp_host,
+        port=whapp_port
+    )
