1d6a44685567b6115ea10fab1d9a57f95edb1135
iamfromhe1l <sgadgsfad@gmail.com>
2022-03-27 18:20:59 +0300

    lkk

--- main1.py
+++ main1.py
@@ -4,6 +4,16 @@ from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher import Dispatcher
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.webhook import SendMessage
+
+
+import aiogram.utils.markdown as md
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ParseMode
+
+
 from aiogram.utils.executor import start_webhook
 import os
 
@@ -21,40 +31,115 @@ whapp_port = os.getenv('PORT', default=8000)
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+
+dp = Dispatcher(bot,storage=storage)
 dp.middleware.setup(LoggingMiddleware())
 
-def get_keybd():
-    buttons = [
-        types.InlineKeyboardButton(text='-1', callback_data='num_dec'),
-        types.InlineKeyboardButton(text='+1', callback_data='num_inc'),
-        types.InlineKeyboardButton(text='Done', callback_data='num_fnsh'),
-    ]
-    keyboard = types.InlineKeyboardMarkup(row_width=2)
-    keyboard.add(*buttons)
-    return keyboard
 
-async def update_text(msg: types.Message, new_value: int):
-    await msg.edit_text(f'Укажите число: {new_value}', reply_markup=get_keybd())
 
-@dp.message_handler(commands='numbers')
-async def cmd_nums(msg: types.Message):
-    user_data[msg.from_user.id] = 0
-    await msg.answer('Укажите число: 0', reply_markup=get_keybd())
+# States
+class Form(StatesGroup):
+    name = State()  # Will be represented in storage as 'Form:name'
+    age = State()  # Will be represented in storage as 'Form:age'
+    gender = State()  # Will be represented in storage as 'Form:gender'
 
-@dp.callback_query_handler(Text(startswith='num_'))
-async def call_num(call: types.CallbackQuery):
-    user_val = user_data.get(call.from_user.id, 0)
-    action = call.data.split('_')[1]
-    if action == 'num_inc':
-        user_data[call.from_user.id] = user_val + 1
-        await update_text(call.message, user_val+1)
-    elif action == 'num_dec':
-        user_data[call.from_user.id] = user_val - 1
-        await update_text(call.message, user_val - 1)
-    elif action == 'num_fnsh':
-        await call.message.edit_text(f'Итого: {user_val}')
-    await call.answer()
+
+@dp.message_handler(commands='start')
+async def cmd_start(message: types.Message):
+    """
+    Conversation's entry point
+    """
+    # Set state
+    await Form.name.set()
+
+    await message.reply("Hi there! What's your name?")
+
+
+# You can use state '*' if you need to handle all states
+@dp.message_handler(state='*', commands='cancel')
+@dp.message_handler(Text(equals='cancel', ignore_case=True), state='*')
+async def cancel_handler(message: types.Message, state: FSMContext):
+    """
+    Allow user to cancel any action
+    """
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+
+    logging.info('Cancelling state %r', current_state)
+    # Cancel state and inform user about it
+    await state.finish()
+    # And remove keyboard (just in case)
+    await message.reply('Cancelled.', reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(state=Form.name)
+async def process_name(message: types.Message, state: FSMContext):
+    """
+    Process user name
+    """
+    async with state.proxy() as data:
+        data['name'] = message.text
+
+    await Form.next()
+    await message.reply("How old are you?")
+
+
+# Check age. Age gotta be digit
+@dp.message_handler(lambda message: not message.text.isdigit(), state=Form.age)
+async def process_age_invalid(message: types.Message):
+    """
+    If age is invalid
+    """
+    return await message.reply("Age gotta be a number.\nHow old are you? (digits only)")
+
+
+@dp.message_handler(lambda message: message.text.isdigit(), state=Form.age)
+async def process_age(message: types.Message, state: FSMContext):
+    # Update state and data
+    await Form.next()
+    await state.update_data(age=int(message.text))
+
+    # Configure ReplyKeyboardMarkup
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add("Male", "Female")
+    markup.add("Other")
+
+    await message.reply("What is your gender?", reply_markup=markup)
+
+
+@dp.message_handler(lambda message: message.text not in ["Male", "Female", "Other"], state=Form.gender)
+async def process_gender_invalid(message: types.Message):
+    """
+    In this example gender has to be one of: Male, Female, Other.
+    """
+    return await message.reply("Bad gender name. Choose your gender from the keyboard.")
+
+
+@dp.message_handler(state=Form.gender)
+async def process_gender(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['gender'] = message.text
+
+        # Remove keyboard
+        markup = types.ReplyKeyboardRemove()
+
+        # And send message
+        await bot.send_message(
+            message.chat.id,
+            md.text(
+                md.text('Hi! Nice to meet you,', md.bold(data['name'])),
+                md.text('Age:', md.code(data['age'])),
+                md.text('Gender:', data['gender']),
+                sep='\n',
+            ),
+            reply_markup=markup,
+            parse_mode=ParseMode.MARKDOWN,
+        )
+
+    # Finish conversation
+    await state.finish()
 
 async def on_startup(dp):
     await bot.set_webhook((wh_url))
