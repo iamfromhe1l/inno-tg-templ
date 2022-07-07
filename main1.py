18f38f4c2b71aabc4d9d6c030778e7af4d7a8204
iamfromhe1l <sgadgsfad@gmail.com>
2022-07-07 19:57:39 +0300

    ch44

--- main1.py
+++ main1.py
@@ -1,18 +1,16 @@
-import logging
-from aiogram import Bot, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.dispatcher import Dispatcher
+from aiogram import Bot, types, Dispatcher
 import requests
-from datetime import datetime
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.utils.executor import start_webhook
+from bs4 import BeautifulSoup as bs
+import asyncio
+import aioschedule
 import os
+import logging
+
 
 TOKEN = os.getenv('BOT_TOKEN')
+URL = os.getenv('URL')
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
-WEATHER_API = os.getenv('WEATHER_API')
 
 wh_host = f'https://{HEROKU_APP_NAME}.herokuapp.com'
 wh_path = f'/webhook/{TOKEN}'
@@ -24,66 +22,43 @@ whapp_port = os.getenv('PORT', default=8000)
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=TOKEN)
-storage = MemoryStorage()
+bot = Bot(TOKEN)
+dp = Dispatcher(bot)
+chatid = '933028899'
 
-dp = Dispatcher(bot,storage=storage)
-dp.middleware.setup(LoggingMiddleware())
+def getCurses(URL):
+    r = requests.get(URL)
+    soup = bs(r.text, "html.parser")
+    divs = soup.find_all("div", {'class':'my-1'})
+    mas = []
+    for e in divs:
+        res = e.text
+        res = res.replace('  ', '').replace('\r', '').replace('\n\n', '')
+        mas.append(res.split('\n'))
+    return mas
+
+def getResult(mas):
+    result = ''
+    sub_res = mas
+    for elem in sub_res:
+        result += ': '.join(elem) + '\n'
+    return result
 
 
-def get_weather(city, weather_api):
-    API_LINK = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api}&units=metric'
-    r = requests.get(API_LINK)
-    return r.json(), r.status_code
+@dp.message_handler()
+async def sch_r():
+    res = getResult(getCurses(URL))
+    await bot.send_message(chat_id=chatid, text=res)
 
-def city_check(city, weather_api):
-    status = get_weather(city, weather_api)[1]
-    return 0 if status != 200 else 1
-
-def decode_weather(w):
-    res = []
-    wm=w['main']
-    res.append(f'Город: {w["name"]}\n')
-    res.append(f'Температура:\n1.Current: {wm["temp"]}°С, 2.Max: {wm["temp_max"]}°С, 3.Min: {wm["temp_min"]}°С\n')
-    res.append(f'Влажность: {wm["humidity"]}%\n')
-    tzsec = w['timezone']
-    sunrise_t = datetime.utcfromtimestamp(w["sys"]["sunrise"]+tzsec).strftime('%Y-%m-%d %H:%M:%S')
-    sunset_t = datetime.utcfromtimestamp(w["sys"]["sunset"]+tzsec).strftime('%Y-%m-%d %H:%M:%S')
-    res.append(f'Время рассвета: {sunrise_t.split()[1]}\nВремя заката: {sunset_t.split()[1]}')
-    return '\n'.join(res)
-
-class Form(StatesGroup):
-    get_city = State()
-    main_st = State()
-
-@dp.message_handler(commands='start', state='*')
-async def get_city_name(message: types.Message):
-    await Form.get_city.set()
-    await message.answer(text='Введите название города', reply_markup=types.ReplyKeyboardRemove())
-
-@dp.message_handler(lambda message: not city_check(message.text, WEATHER_API), state=Form.get_city)
-async def wrong_city(message: types.Message):
-    await message.reply(text='Вы ввели неправильный город')
-
-@dp.message_handler(lambda message: city_check(message.text, WEATHER_API), state=Form.get_city)
-async def set_city(message: types.Message, state=FSMContext):
-    await Form.main_st.set()
-    city = message.text
-    kbd= types.ReplyKeyboardMarkup()
-    btn = types.KeyboardButton('Вывести погоду')
-    kbd.add(btn)
-    async with state.proxy() as data:
-        data['def_city'] = city
-        await message.reply(text='Город по умолчанию установлен\nВведите /start, чтобы выбрать другой город', reply_markup=kbd)
-
-@dp.message_handler(text='Вывести погоду', state=Form.main_st)
-async def print_weather(message: types.Message, state=FSMContext):
-    async with state.proxy() as data:
-        await message.answer(decode_weather(get_weather(data['def_city'],WEATHER_API)[0]))
+async def scheduler():
+    aioschedule.every().hour.do(sch_r)
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
 
 async def on_startup(dp):
-    await bot.set_webhook((wh_url))
-    
+    asyncio.create_task(scheduler())
+
 async def on_shutdown(dp):
     logging.warning('turning off')
     
@@ -91,7 +66,7 @@ async def on_shutdown(dp):
     await dp.storage.close()
     
     logging.warning('bye')
-    
+
 if __name__ == '__main__':
     start_webhook(
         dispatcher=dp,
